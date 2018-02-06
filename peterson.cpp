9184ee0f6dacfce71b0dee4d3a0a0a51e6978179
mikalp <Mikhail.Alpinskiy@transas.com>
2018-02-06 16:09:40 +0300

    fixed
    thanks to
    https://bartoszmilewski.com/2008/12/01/c-atomics-and-memory-ordering/

--- peterson.cpp
+++ peterson.cpp
@@ -5,20 +5,20 @@
 class Lock {
  public:
     Lock() {
-        flags_[0].store(false, std::memory_order::memory_order_seq_cst);
-        flags_[1].store(false, std::memory_order::memory_order_seq_cst);
+        flags_[0].store(false, std::memory_order::memory_order_release);
+        flags_[1].store(false, std::memory_order::memory_order_release);
     }
 
     void acquire(int pid) {
-        flags_[pid].store(true, std::memory_order::memory_order_seq_cst);
-        after_you_.store(pid, std::memory_order::memory_order_seq_cst);
-        for (; flags_[1 - pid].load(std::memory_order::memory_order_seq_cst) &&
-               after_you_.load(std::memory_order::memory_order_seq_cst) == pid;)
+        flags_[pid].store(true, std::memory_order::memory_order_release);
+        after_you_.store(pid, std::memory_order::memory_order_acq_rel);
+        for (; flags_[1 - pid].load(std::memory_order::memory_order_acquire) &&
+               after_you_.load(std::memory_order::memory_order_acquire) == pid;)
             ;
     }
 
     void release(int pid) {
-        flags_[pid].store(false, std::memory_order::memory_order_seq_cst);
+        flags_[pid].store(false, std::memory_order::memory_order_release);
     }
 
  private:
@@ -27,16 +27,16 @@ class Lock {
 };
 
 int cnt = 0;
-Lock lock;
+Lock cnt_lock;
 
 void run(int pid) {
     int const upper_bound = 10000000;
     static_assert(upper_bound < std::numeric_limits<int>::max() / 2, "");
 
     for (int i = 0; i < upper_bound; ++i) {
-        lock.acquire(pid);
+        cnt_lock.acquire(pid);
         ++cnt;
-        lock.release(pid);
+        cnt_lock.release(pid);
     }
 }
 
